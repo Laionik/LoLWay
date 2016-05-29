f85d6e488642bc5c085c8d7453720a35204d7eb6
Laionik <lukasz.sosinski4@gmail.com>
2016-05-29 11:50:57 +0200

    Send Grid

--- LoLWay/Controllers/HomeController.cs
+++ LoLWay/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Web.Mvc;
 using RiotAPI;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LoLWay.Controllers
 {
     public class HomeController : Controller
     {
+        private string apiKey = "09e2ba8a-34b8-44d4-9445-262ed673da5d";
         public ActionResult Index()
         {
             if (User.Identity.IsAuthenticated)
@@ -25,9 +27,8 @@ namespace LoLWay.Controllers
         [HttpPost]
         public ActionResult Index(string nickname, string region)
         {
-            string apiKey = "09e2ba8a-34b8-44d4-9445-262ed673da5d";
-            var summonersList = Summoner.GetSummonersByName(apiKey, new List<string>() { nickname }, region);
-            var currentGame = CurrentGame.GetCurrentGame(apiKey, summonersList[0].id, region);
+            var summonersList = Summoner.GetSummonerByName(apiKey, nickname, region);
+            var currentGame = CurrentGame.GetCurrentGame(apiKey, summonersList.id, region);
             // wyciągnij informacje o graczach
             return View(currentGame);
         }
