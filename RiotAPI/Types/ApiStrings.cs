a1098c651c56aab73e84ce3bfc1e573f96f1cc3f
M4sq <wojciech.sycz@gmail.com>
2016-05-29 12:07:41 +0200

    Added GetApiKey method

--- RiotAPI/Types/ApiStrings.cs
+++ RiotAPI/Types/ApiStrings.cs
@@ -11,5 +11,17 @@ namespace RiotAPI.Types
         public static String GetSummonerByName = "/v1.4/summoner/by-name/";
 
         public static String GetSummonerByID = "/v1.4/summoner/";
+
+        public static List<String> apiKeys = new List<string>()
+        {
+           "10a16ba0-b815-4a5f-971a-321491238daa", // username: api753 mail: vktzycrz@sharklasers.com
+           "a7d2868b-084a-44aa-acd5-7e35fec86d1c", // mail: vbnzycrz@sharklasers.com
+           "7efa3781-f76c-4b67-a29b-fc2bfce28144", // mail: vbnbecrz@sharklasers.com
+           "23c6da01-abfc-4dff-992f-bf4998fea935", //mail: jknbecrz@sharklasers.com
+           "76d626dc-5500-4202-b88c-9f2d875eb3b9", //mail: pijbecrz@sharklasers.com
+           "9bec93b3-48ff-40cc-947c-21951e608d76", // mail: pihytcrz@sharklasers.com
+           "e7f5e085-4c2f-463f-b7c9-5944d8c54c1b", //mail: pihytbgz@sharklasers.com
+           "60d81102-841b-4dce-811c-daac945bd670" //mail: pihytbre@sharklasers.com
+        };
     }
 }
