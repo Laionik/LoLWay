69f2d0c13dd1295971d4ba28be9ee013ee0b0783
Laionik <lukasz.sosinski4@gmail.com>
2016-06-22 16:53:30 +0200

    Twitter update

--- LoLWay/App_Start/Startup.Auth.cs
+++ LoLWay/App_Start/Startup.Auth.cs
@@ -7,6 +7,7 @@ using Microsoft.Owin.Security.Google;
 using Owin;
 using LoLWay.Models;
 using Microsoft.Owin.Security.Twitter;
+using Microsoft.Owin.Security;
 
 namespace LoLWay
 {
@@ -52,9 +53,21 @@ namespace LoLWay
             //    clientId: "048fa336-afa7-4ee4-9086-46849080617c",
             //    clientSecret: "AO0oYPLqgpjrjbSBqDoJ8eF");
 
-            app.UseTwitterAuthentication(
-               consumerKey: "9fzgJrAF0GYJDkojknzPydxJE	",
-               consumerSecret: "9KaBO3lRuFXZ4cM21pkbSI7JiRHiaAP0ZFVDuIbNU9K8ILKu9");
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = "9fzgJrAF0GYJDkojknzPydxJE",
+                ConsumerSecret = "9KaBO3lRuFXZ4cM21pkbSI7JiRHiaAP0ZFVDuIbNU9K8ILKu9e",
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
+                {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                    "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                    "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
+                    "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
+                })
+            });
+
 
             app.UseFacebookAuthentication(
                appId: "1760553390831603",
